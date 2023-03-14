546f898513ec649db81e6d0bd58f18c45c0166a9
EgeKale <127881949+EgeKale@users.noreply.github.com>
2023-03-15 02:07:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+liste= [4,1,2,5,7,3,27,123,66,899,6]
+
+for item in liste:
+    if type(item) == str:
+        print('Sayilarin Oldugu Bir Liste Giriniz!!')
+        break
+
+else:
+    liste.sort()
+    a=(liste[0])
+    b=(liste[-1])
+    print(a+b)
+
+
+
+
+
+
+
+
+# for item in liste:
+#     if type(item) == str:
+#         print('Sayilarin Oldugu Bir Liste Giriniz!!')
+
+
